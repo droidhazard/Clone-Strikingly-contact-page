5d220d3b5b0ed40b0d8c3a7176e8d817497c4ffa
Zohaib Ali <www.zohaib.0335@gmail.com>
2022-11-04 15:09:06 +0500

    modified file

--- javascript.js
+++ javascript.js
@@ -1,13 +1,20 @@
-let navToggled = false;
 const body = document.getElementById("body");
+const sidebar = document.getElementById("sidebar");
+
+// Initialize
+let navToggled = false;
+body.style.left = "0";
+sidebar.style.right = "-50%";
+
 function toggleNav() {
-  if (navToggled==false) {
-    console.log(body);
+  if (navToggled == false) {
     body.style.left = "-50%";
+    sidebar.style.right = "0";
     navToggled = true;
   }
   else {
     body.style.left = "0";
+    sidebar.style.right = "-50%";
     navToggled = false;
   }
 }
